d0b9e8bcf385bebd11466119addad1204dfc3848
sangam638kamble <sangamkamble636@yahoo.com>
2019-03-19 01:29:56 +0530

    First c program

---
+++ HelloFriends.c
@@ -0,0 +1,8 @@
+public class HelloFriends {
+
+         public static void main(String[] args) {
+              // Prints "Hello, Friends" to the terminal window.
+              System.out.println("Hello, Friends");
+         }
+
+}
